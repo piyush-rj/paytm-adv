79cd0350e4857dc2b451dcf5b05c876ee4f776a0
PiyushRaj2610 <piyushraj26102004@gmail.com>
2025-04-06 20:06:41 +0530

    init

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
-import { AppbarClient } from "../components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>
-          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
-            <AppbarClient />
-            {children}
-          </div>
-        </body>
+        <body className={inter.className}>{children}</body>
       </Providers>
     </html>
   );
--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,66 +1,65 @@
-import db from "@repo/db/client";
+import db from "@repo/db/client"
 import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from "bcrypt";
+import bcrypt from "bcrypt"
 
 export const authOptions = {
-    providers: [
-      CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
-            password: { label: "Password", type: "password", required: true }
-          },
-          // TODO: User credentials type from next-aut
-          async authorize(credentials: any) {
-            // Do zod validation, OTP validation here
-            const hashedPassword = await bcrypt.hash(credentials.password, 10);
-            const existingUser = await db.user.findFirst({
-                where: {
-                    number: credentials.phone
-                }
-            });
-
-            if (existingUser) {
-                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
-                if (passwordValidation) {
-                    return {
-                        id: existingUser.id.toString(),
-                        name: existingUser.name,
-                        email: existingUser.number
-                    }
-                }
-                return null;
-            }
-
-            try {
-                const user = await db.user.create({
-                    data: {
-                        number: credentials.phone,
-                        password: hashedPassword
+    providers :[
+        CredentialsProvider({
+            name: "Phone Number",
+            credentials: {
+                phone: { label: "Phone number", type: "text", placeholder:"enter number"},
+                password: { label: "Password", type:"password", placeholder:"enter password"}
+            },
+            async authorize (credentials: any) {
+                // zod and otp validation
+                const hashedPassword = await bcrypt.hash(credentials.password, 10)
+                const existingUser = await db.user.findFirst({
+                    where: {
+                        number: credentials.phone
                     }
                 });
-            
-                return {
-                    id: user.id.toString(),
-                    name: user.name,
-                    email: user.number
-                }
-            } catch(e) {
-                console.error(e);
-            }
 
-            return null
-          },
+                if(existingUser){
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password)
+                    if(passwordValidation){
+                        return {
+                            id: existingUser.id.toString(),
+                            name: existingUser.name,
+                            email: existingUser.number
+                        }
+                    } else {
+                        throw new Error(" incorrect password ")
+                    }
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data: {
+                            number: credentials.phone,
+                            password: hashedPassword
+                        }
+                    });
+
+                    return {
+                        id: user.id.toString(),
+                        name: user.name,
+                        number: user.number
+                    }
+                } catch (error) {
+                    console.error(error)
+                }
+                return null
+            }
         })
     ],
+
     secret: process.env.JWT_SECRET || "secret",
     callbacks: {
-        // TODO: can u fix the type here? Using any is bad
+
         async session({ token, session }: any) {
             session.user.id = token.sub
 
-            return session
+            return session;
         }
     }
-  }
-  
+}

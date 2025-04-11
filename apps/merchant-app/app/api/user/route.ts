4dfa5249551bc89be8e210716bd7f9c324ac6bec
PiyushRaj2610 <piyushraj26102004@gmail.com>
2025-04-11 15:43:50 +0530

    update

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server"
-import { PrismaClient } from "@repo/db/client";
-
-const client = new PrismaClient();
+import db from "@repo/db/client"
 
 export const GET = async () => {
-    await client.user.create({
+    await db.user.create({
         data: {
-            email: "asd",
-            name: "adsads"
+            email: "asd@example.com",
+            name: "adsads",
+            number: "1234567890", // Add this required field
+            password: "somehashedpassword" // Add this required field
         }
     })
     return NextResponse.json({
--- apps/user-app/app/api/user/user.route.ts
+++ apps/user-app/app/api/user/user.route.ts
@@ -4,14 +4,17 @@ import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
     const session = await getServerSession(authOptions);
-    if (session.user) {
+    
+    // Check if session exists before accessing session.user
+    if (session && session.user) {
         return NextResponse.json({
             user: session.user
-        })
+        });
     }
+    
     return NextResponse.json({
         message: "You are not logged in"
     }, {
         status: 403
-    })
+    });
 }
